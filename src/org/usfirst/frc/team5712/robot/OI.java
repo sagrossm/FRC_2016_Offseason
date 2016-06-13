5be25a9171eb2d66605792a753618ed8479742ee
Team5712 <lockwood@hemlockps.com>
2016-06-13 09:47:05 -0400

    Add files via upload

--- src/org/usfirst/frc/team5712/robot/OI.java
+++ src/org/usfirst/frc/team5712/robot/OI.java
@@ -20,60 +20,60 @@ public class OI {
 
 	//Buttons
 	//driveStick Buttons (Driver 1):
-	public Button shiftGear_DriveStickTrigger; // = new JoystickButton(driveStick, 1);
+	public Button shiftGear_DriveStickTrigger; //1
 	
-	public Button invertMotors_DriveStickButton; // = new JoystickButton(driveStick, 3);
-	public Button normalizeMotors_DriveStickButton; // = new JoystickButton(driveStick, 4);
+	public Button invertMotors_DriveStickButton; //3
+	public Button normalizeMotors_DriveStickButton; //4
 	
-	public Button turnXdegrees_DriveStickButton; // = new JoystickButton(driveStick, 6); 
+	public Button turnXdegrees_DriveStickButton; //6 
 	
-	public Button solenoidOut_DriveStickButton; // = new JoystickButton(driveStick, 7);
-	public Button solenoidIn_DriveStickButton; // = new JoystickButton(driveStick, 8);
+	public Button solenoidOut_DriveStickButton; //7
+	public Button solenoidIn_DriveStickButton; //8
 	
 	//shootStick Buttons (Driver 2):
-	public Button shoot_ShootStickTrigger; // = new JoystickButton(shootStick, 1);
-	public Button intake_ShootStickButton; // = new JoystickButton(shootStick, 2);
+	public Button shoot_ShootStickTrigger; //1
+	public Button intake_ShootStickButton; //2
 	
-	public Button adjustArmUpEncoder_ShootStickButton; // = new JoystickButton(shootStick, 5);
-	public Button adjustArmDownEncoder_ShootStickButton; // = new JoystickButton(shootStick, 6);
+	public Button adjustArmUpEncoder_ShootStickButton; //5
+	public Button adjustArmDownEncoder_ShootStickButton; //6
 	
-	public Button solenoidOut_ShootStickButton; // = new JoystickButton(shootStick, 10);
-	public Button solenoidIn_ShootStickButton; // = new JoystickButton(shootStick, 11);
+	public Button solenoidOut_ShootStickButton; //10
+	public Button solenoidIn_ShootStickButton; //11
 	
 	public OI() {
 		
 		//driveStick Button Commands
 		shiftGear_DriveStickTrigger = new JoystickButton(driveStick, 1);
-		shiftGear_DriveStickTrigger.whileHeld(new ShiftGear()); //1
+		shiftGear_DriveStickTrigger.whileHeld(new ShiftGear());
 		
 		invertMotors_DriveStickButton = new JoystickButton(driveStick, 3);
-		invertMotors_DriveStickButton.whenPressed(new InvertMotorsTrue()); //3
+		invertMotors_DriveStickButton.whenPressed(new InvertMotorsTrue());
 		normalizeMotors_DriveStickButton = new JoystickButton(driveStick, 4);
-		normalizeMotors_DriveStickButton.whenPressed(new InvertMotorsFalse()); //4
+		normalizeMotors_DriveStickButton.whenPressed(new InvertMotorsFalse());
 		
 		turnXdegrees_DriveStickButton = new JoystickButton(driveStick, 6);
-		turnXdegrees_DriveStickButton.whenPressed(new TurnXdegrees()); //6
+		turnXdegrees_DriveStickButton.whenPressed(new TurnXdegrees());
 		
 		solenoidOut_DriveStickButton = new JoystickButton(driveStick, 7);
-		solenoidOut_DriveStickButton.whenPressed(new SolenoidOut()); //7
+		solenoidOut_DriveStickButton.whenPressed(new SolenoidOut());
 		solenoidIn_DriveStickButton = new JoystickButton(driveStick, 8);
-		solenoidIn_DriveStickButton.whenPressed(new SolenoidIn()); //8
+		solenoidIn_DriveStickButton.whenPressed(new SolenoidIn());
 		
 		//shootStick Button Commands
 		shoot_ShootStickTrigger = new JoystickButton(shootStick, 1);
-		shoot_ShootStickTrigger.whenPressed(new Shoot()); //1
+		shoot_ShootStickTrigger.whenPressed(new Shoot());
 		intake_ShootStickButton = new JoystickButton(shootStick, 2);
-		intake_ShootStickButton.whenPressed(new Intake()); //2
+		intake_ShootStickButton.whenPressed(new Intake());
 		
 		adjustArmUpEncoder_ShootStickButton = new JoystickButton(shootStick, 5);
-		adjustArmUpEncoder_ShootStickButton.whenPressed(new AdjustArmUpEncoder()); //5
+		adjustArmUpEncoder_ShootStickButton.whenPressed(new AdjustArmUpEncoder());
 		adjustArmDownEncoder_ShootStickButton = new JoystickButton(shootStick, 6);
-		adjustArmDownEncoder_ShootStickButton.whenPressed(new AdjustArmDownEncoder()); //6
+		adjustArmDownEncoder_ShootStickButton.whenPressed(new AdjustArmDownEncoder());
 		
 		solenoidOut_ShootStickButton = new JoystickButton(shootStick, 10);
-		solenoidOut_ShootStickButton.whenPressed(new SolenoidOut()); //10
+		solenoidOut_ShootStickButton.whenPressed(new SolenoidOut());
 		solenoidIn_ShootStickButton = new JoystickButton(shootStick, 11);
-		solenoidIn_ShootStickButton.whenPressed(new SolenoidIn()); //11
+		solenoidIn_ShootStickButton.whenPressed(new SolenoidIn());
 		
 	}
 }
